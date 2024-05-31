7d0b63cdd3db7bc4b60a2efde10496c24f2cf939
leekyuwon <sjin9574@naver.com>
2024-05-31 23:43:47 +0900

    javaMid

---
+++ java-mid/src/enumeration/ex1/StringGradeEx1_2.java
@@ -0,0 +1,29 @@
+package enumeration.ex1;
+
+public class StringGradeEx1_2 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+
+//        discountService.discount()
+
+        // 존재하지 않는 등급 ( 잘못된 등급 기입 )
+        int vip = discountService.discount("VIP", price);
+        System.out.println("VIP 등급의 할인 금액 = " + vip);
+
+        // 오타
+        int diamodd = discountService.discount("DIAMODD", price);
+        System.out.println("DIAMODD 등급의 할인 금액 = " + diamodd);
+
+        // 소문자 입력
+        int gold = discountService.discount("gold", price);
+        System.out.println("gold 등급의 할인 금액 = " + gold);
+
+        /**
+         * * 타입 안정성 부족
+         * * 데이터 일관성 문제
+         * */
+    }
+}
--- java-mid/src/lang/math/test/LottoMain.java
+++ java-mid/src/lang/math/test/LottoMain.java
@@ -6,7 +6,8 @@ public class LottoMain {
 
         LottoGenerator lottoGenerator = new LottoGenerator();
         int[] spin = lottoGenerator.spin();
-
+        Integer integer = Integer.valueOf(10);
+        int intValue = integer.intValue();
         System.out.print("로또 번호: ");
         for (int i = 0; i < spin.length; i++) {
             System.out.print(spin[i] + " ");
